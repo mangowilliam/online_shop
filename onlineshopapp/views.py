b20bc42a620703830879c025b3822e0c4eb880d4
williammango <williammango2015@gmail.com>
2019-08-14 14:50:23 +0300

    added dataase

--- onlineshopapp/views.py
+++ onlineshopapp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def home(request):
+       
+    return render(request, "home.html")
+    
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form =UserRegistrationForm()
+    return render(request,'registration/registration_form.html',{'form':form}) 
